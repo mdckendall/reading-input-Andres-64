5c3abaab97589f5e1052792796fe5129be15f427
Andres Garcia <111749792+Andres-64@users.noreply.github.com>
2022-09-21 14:09:17 +0000

    homework

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Enter a word:");
-    
-    //Your code will return the length of the entered String
-  }
+    public static void main(String[] args) {  
+        System.out.println("Enter a word:");
+        Scanner myWord = new Scanner(System.in);
+      
+        String word = myWord.nextLine();
+      
+        int stringLength = word.length();
+        System.out.println(stringLength);
+    }
 }
